9e9b8851109dfdaf7c1357a3616e9061abf0f0b0
AstroSnow <b.snow@exeter.ac.uk>
2022-05-09 06:19:53 +0100

    Put a check for time step changes across iterations

--- main.f90
+++ main.f90
@@ -12,13 +12,15 @@ program main
   !====================================================================
   !  use globalvar,only:U_h,U_m,nt,time,dt,flag_stop,tend,nmax
   use globalvar,only:U_h,U_m,nt,time,dt,flag_stop,tend,nmax,nout,my_rank, flag_IR,flag_PIP,&
-	esav,emsavtime  
+	esav,emsavtime,U_m_backup,U_h_backup
   use initial_rot,only:prologue
   use io_rot,only:output,stop_sim,epilogue
   use solver_rot,only:run_solver
   use scheme_rot,only:cfl
   use pip_rot,only:set_IR
   implicit none
+  double precision::dtnew,dtmod
+  dtmod=1.d0
   !----------------------------------------------------------------------|
   ! initial setting
   !----------------------------------------------------------------------|
@@ -31,19 +33,38 @@ program main
        call set_IR(U_h,U_m)       
     endif
      ! calculate cfl condition  -----------------------------------------
-     call cfl(U_h,U_m)          
+     call cfl(U_h,U_m,dt)
+     dt=dt*dtmod
+     !Back up the array
+     U_m_backup=U_m
+     U_h_backup=U_h  
      ! Time intergration  -----------------------------------------------
-     call run_solver(U_h,U_m) 
-     ! Output of simulation data  ---------------------------------------
-     if (dt.ne.dt) print *, my_rank,dt
-     time=time+dt !! NB: dt can be modified by STS routines
-     call output(0)
-     ! Error check  -----------------------------------------------------
-     call stop_sim
-     if (flag_stop .eq.1) then
-        call output(1)
-        exit
+     call run_solver(U_h,U_m)
+     !Check change in dt
+     if((flag_IR.ge.1) .and. (flag_pip.ge.1)) then
+       call set_IR(U_h,U_m)       
      endif
+     call cfl(U_h,U_m,dtnew)
+     !print*,maxval(U_m(:,:,:,2)),maxval(U_m_backup(:,:,:,2))
+     !print*,dt,dtnew
+     if (dtnew .lt. dt/10.d0) then
+     	dtmod=0.1d0*dtmod
+        if (my_rank .eq. 0) print *, 'Reducing dt by ',dtmod,time
+        U_m=U_m_backup
+        U_h=U_h_backup
+     else 
+		 ! Output of simulation data  ---------------------------------------
+		 if (dt.ne.dt) print *, my_rank,dt
+		 time=time+dt !! NB: dt can be modified by STS routines
+		 call output(0)
+		 dtmod=1.d0
+	     ! Error check  -----------------------------------------------------
+		 call stop_sim
+		 if (flag_stop .eq.1) then
+		    call output(1)
+		    exit
+		 endif
+	 endif
   enddo
   !----------------------------------------------------------------------|
   ! End of simulation
