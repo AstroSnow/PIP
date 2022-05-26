bf14b9da7e209fe7da23d124dc52bfad02961b68
AstroSnow <b.snow@exeter.ac.uk>
2022-05-26 10:19:12 +0100

    Correction to radiative recombination rates

--- expintcalc/radexpintcalc.f90
+++ expintcalc/radexpintcalc.f90
@@ -1,7 +1,7 @@
 PROGRAM radexpintcalc
 	IMPLICIT NONE
-	double precision::sol,oldsol,diff,Trad,sol2,Telec
-	double precision::nuarr,exf,dion,ionval,ionmin,ionmax
+	double precision::sol,oldsol,diff,Trad,sol2,Telec,sol3,sol4,sol5,sol6
+	double precision::nuarr(5),exf,dion,ionval,ionmin,ionmax
 	double precision::recmin,recmax,drec,recval
 	double precision,parameter::cli=299792458.d0 !Speed of light in m/s
 	double precision,parameter::kboltz=1.38064852e-23 !Boltzmann Constant [m^2 kg s^-2 K^-1]
@@ -21,10 +21,20 @@ PROGRAM radexpintcalc
 
 	dion=(ionmax-ionmin)/(nsamps)
 
-	recmin=h*cli/121.57e-9/kboltz/100000.d0
-	recmax=h*cli/2279.0e-9/kboltz/100.d0
+	recmin=h/kboltz/100000.d0
+	recmax=h/kboltz/100.d0
     
     drec=(recmax-recmin)/(nsamps)
+    
+    nuarr(1)=cli/91.175e-9 !Continuum
+    !Balmer series
+    nuarr(2)=cli/364.6e-9 !Continuum
+    !Pachen series
+    nuarr(3)=cli/820.4e-9 !Continuum
+    !Brackett series
+    nuarr(4)=cli/1458.0e-9 !Continuum
+    !Pfund series
+    nuarr(5)=cli/2279.0e-9 !Continuum
     
 	do ti=0,nsamps
 ! 		Trad=ti*100.d0
@@ -34,7 +44,8 @@ PROGRAM radexpintcalc
 
 		ionval=ionmin+ti*dion
 		recval=recmin+ti*drec
-	 
+	 	!print*,recval
+	 	!stop
 	 	!IONISATION VALUES
 		sol=0.d0
 		oldsol=sol
@@ -54,20 +65,73 @@ PROGRAM radexpintcalc
 !USE EQUATION 3.12 in Sollum to simplify!	 
 		!Recombination rates    
 		sol2=0.d0
-		call expintrutton1(recval,sol2)
+		call expintrutton1(recval*nuarr(1),sol2)
 		oldsol=sol2
 		diff=1.0d0
 		iii=1
-		do while (diff .GT. 0.0001)
+		do while (diff .GT. 0.000001)
 		    oldsol=sol2
-		    call expintrutton1(dble(iii)*h*nuarr/kboltz/Trad+recval,exf)
+		    call expintrutton1(dble(iii)*h*nuarr(1)/kboltz/Trad+recval*nuarr(1),exf)
 		    sol2=sol2+exf
 		    diff=abs((sol2-oldsol)/sol2)
 		    iii=iii+1
 		enddo
 				
+		sol3=0.d0
+		call expintrutton1(recval*nuarr(2),sol3)
+		oldsol=sol3
+		diff=1.0d0
+		iii=1
+		do while (diff .GT. 0.000001)
+		    oldsol=sol3
+		    call expintrutton1(dble(iii)*h*nuarr(2)/kboltz/Trad+recval*nuarr(2),exf)
+		    sol3=sol3+exf
+		    diff=abs((sol3-oldsol)/sol3)
+		    iii=iii+1
+		enddo
 				
-	   write(1,*) ionval, sol,recval,sol2
+		sol4=0.d0
+		call expintrutton1(recval*nuarr(3),sol4)
+		oldsol=sol4
+		diff=1.0d0
+		iii=1
+		do while (diff .GT. 0.000001)
+		    oldsol=sol4
+		    call expintrutton1(dble(iii)*h*nuarr(3)/kboltz/Trad+recval*nuarr(3),exf)
+		    sol4=sol4+exf
+		    diff=abs((sol4-oldsol)/sol4)
+		    iii=iii+1
+		enddo
+				
+				
+		sol5=0.d0
+		call expintrutton1(recval*nuarr(4),sol5)
+		oldsol=sol5
+		diff=1.0d0
+		iii=1
+		do while (diff .GT. 0.000001)
+		    oldsol=sol5
+		    call expintrutton1(dble(iii)*h*nuarr(4)/kboltz/Trad+recval*nuarr(4),exf)
+		    sol5=sol5+exf
+		    diff=abs((sol5-oldsol)/sol5)
+		    iii=iii+1
+		enddo
+				
+		sol6=0.d0
+		call expintrutton1(recval*nuarr(5),sol6)
+		oldsol=sol6
+		diff=1.0d0
+		iii=1
+		do while (diff .GT. 0.000001)
+		    oldsol=sol6
+		    call expintrutton1(dble(iii)*h*nuarr(5)/kboltz/Trad+recval*nuarr(5),exf)
+		    sol6=sol6+exf
+		    diff=abs((sol6-oldsol)/sol6)
+		    iii=iii+1
+		enddo
+				
+				
+	   write(1,*) ionval, sol,recval,sol2,sol3,sol4,sol5,sol6
 	 enddo
 	 
 	 close(1) 
