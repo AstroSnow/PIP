16f332287e70861a26378c91e8f8c5b547da0abf
snow <b.snow@exeter.ac.uk>
2022-08-29 16:51:46 +0100

    Removed print statements

--- MPI_rot.f90
+++ MPI_rot.f90
@@ -504,9 +504,9 @@ contains
   end subroutine end_mpi
 
   subroutine my_mpi_barrier
-  print*,'calling MPI barrier'
+!  print*,'calling MPI barrier'
     call MPI_barrier(MPI_COMM_WORLD)
-  print*,'called MPI barrier'
+!  print*,'called MPI barrier'
   end subroutine my_mpi_barrier  
 
 end module mpi_rot
