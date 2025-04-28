1ca8fd414e3a9b57c1150b34632bb5e9ea4036d2
AstroSnow <b.snow@exeter.ac.uk>
2025-04-28 15:13:41 +0100

    Added some comments

--- KHtube.f90
+++ KHtube.f90
@@ -32,6 +32,9 @@ subroutine KHtube
   double precision::Nexciteup(n_levels+1),Nexcitedown(n_levels+1),Eion(6)
   double precision,parameter::kbhat=1.38064852,mehat=9.10938356,hhat=6.62607004
   double precision,parameter::kboltz=1.38064852e-23 !Boltzmann Constant [m^2 kg s^-2 K^-1]
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!random number generator
     data seed /123456789, 987654321/
     data hiseed /-1, -1/
     data zseed /0, 0/
@@ -44,12 +47,9 @@ subroutine KHtube
    CALL RANDOM_SEED(PUT=new(1:size))
    call random_number(HARVEST=harvest)
 
-!Set the reference values
-!Test values
-!T0down=7319.689479843136d0
-!T0up=5500.d0
-!n0up=7.5e16
-!n0down=n0up*10.d0
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!Set the reference values for temperature [K] and electron number density [m^-3]
+
 !small jump
 !T0down=6555.84250200805d0
 !T0up=5500.d0
@@ -65,7 +65,10 @@ n0down=n0up*30.d0
 !T0up=6000.d0
 !n0up=7.5e16
 !n0down=n0up*1.d0
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!Flag to make sure I have the right ionisation routine
 if(flag_IR .ne. 4) then
 	print*,'set flag_IR=4 for this routine'
 	!stop
@@ -73,15 +76,23 @@ endif
   
 if (my_rank.eq.0) print*,'Calculating LTE excitation state'
 
-!Allocate arrays
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!Allocate arrays 
+!This is only used for the n_level stuff
+!Should be within a flag or deleted for MHD to save memory
 allocate(Nexcite(ix,jx,kx,n_levels+1)) !Allocate the fractional array
 allocate(Colrat(ix,jx,kx,n_levels+1,n_levels+1))
 call expintread
 if (flag_rad .ge. 2) allocate(radrat(ix,jx,kx,n_levels+1,n_levels+1))
 
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!Ionisation energies
 Eion=[13.6,3.4,1.51,0.85,0.54,0.0] !in eV
 Eion=Eion/13.6*2.18e-18 !Convert to joules (to be dimensionally correct)
 
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!Calculate the LTE level populations using Saha-Boltzman
 Nexciteup(n_levels+1)=n0up
 do i=1,n_levels
 Nexciteup(i)=(2.d0/n0up/2.d0*(2.d0*pi*mehat*kbhat*T0up/hhat/hhat*1.0e14)**(3.d0/2.d0)*exp(-Eion(i)/kboltz/T0up))
@@ -98,8 +109,10 @@ Nexcitedown(1:n_levels)=n0down/Nexcitedown(1:n_levels)
 Nexcitedown=Nexcitedown/n0down
 Nexcitedown=Nexcitedown/sum(Nexcitedown(:))
 
-f_nup=sum(Nexciteup(1:n_levels))/(sum(Nexciteup(1:n_levels))+Nexciteup(n_levels+1))
-f_pup=1.d0-f_nup!Nexcite(1,1,1,6)
+
+!Calculate the xion fraction and pressure ratio for the LTE state
+f_nup=sum(Nexciteup(1:n_levels))/(sum(Nexciteup(1:n_levels))+Nexciteup(n_levels+1)) !neutral fraction
+f_pup=1.d0-f_nup   !Ion fraction
 nnup=n0up/f_pup-n0up
 pnup=nnup*T0up*3.d0/5.d0!f_nup/(f_nup+2.0d0*f_pup)
 ppup=n0up*T0up*6.d0/5.d0
@@ -128,6 +141,12 @@ f_p_pdown=ppdown/n0up!(pnup+ppup)
   call set_coordinate(start,end)
   !---------------------------------------
 
+!Boundary conditions. 
+!flag_bnd(1) is left
+!flag_bnd(2) is right
+!flag_bnd(3) is top
+!flag_bnd(4) is bottom
+!Values:  1 (periodic), 2(symmetric).......
   !!default boundary condition----------------------
   if (flag_bnd(1) .eq.-1) flag_bnd(1)=1
   if (flag_bnd(2) .eq.-1) flag_bnd(2)=1
@@ -139,14 +158,16 @@ f_p_pdown=ppdown/n0up!(pnup+ppup)
 
   !!!========================================================
   !density of lower fluid is unity
-  ro_l=nnup+n0up
+  ro_l=nnup+n0up !total (plasma + neutral) density
   ro_u=nndown+n0down
 
-  vx_l=ro_u/(ro_u+ro_l)*dsqrt(1.d0/10.d0)
+
+
+  vx_l=ro_u/(ro_u+ro_l)*dsqrt(1.d0/10.d0) !Velocity (unused here)
 !  if(flag_mhd.eq.1) then
 !  vx_l=vx_l*sqrt(1.d0+2.d0/(gm*beta))
 !  endif
-  vx_u=-vx_l*ro_l/ro_u
+  vx_u=-vx_l*ro_l/ro_u !Shear velocity
   w_lay=0.003d0
   w_lay=0.1d0
 
@@ -157,7 +178,8 @@ f_p_pdown=ppdown/n0up!(pnup+ppup)
   Nexcitedown(n_levels+1)=Nexcitedown(n_levels+1)*(nndown+n0down)
 
   theta=2.d0*pi*0.d0/360.d0
-  
+
+!Calculate the initial magentic field strength based on the plasma-beta given in settings file  
   if (flag_pip.eq.0) then
      ptot=ppdown
      b0=sqrt(2.0d0/(ptot/ppup/5.0*3.0*beta))
@@ -166,46 +188,35 @@ f_p_pdown=ppdown/n0up!(pnup+ppup)
      b0=sqrt(2.0d0/(ptot/ppup/5.0*3.0*beta))
      print*,'check total pressure for two fluid case'
   endif
-!  if(flag_mhd.eq.1) then
-!     !b0=sqrt(2.0d0/(gm*beta))
-!     b0=sqrt(2.0d0/(ptot/ppup/5.0*3.0*beta))
-!     !B_z=B0*cos(theta)
-!     !B_x=B0*sin(theta)
-!     !B_y=0.0d0
-!  endif
   b0down=b0
-  !b0up=dsqrt(2.0*(0.5*b0down**2+ppdown/5.0*3.0/ppup - 3.0/5.0))! dsqrt(ppdown/ppup)
+
 
 print*,ppup,ppdown
 print*,b0up,b0down
-!stop
 
-!  P_h(:,:,:)=1.0/gm*f_p_n
-!  P_m(:,:,:)=1.0/gm*f_p_p
-
-!  P_h(:,:,:)=2.5d0*f_p_n
-!  P_m(:,:,:)=2.5d0*f_p_p
-
+!Set velocity arrays to zero
   vy_h=0.0d0;vz_h=0.0d0
   vy_m=0.0d0;vz_m=0.0d0
 
-!  print *, 'done', my_rank
 
-  r0=0.5
-  v0=0.01
+  r0=0.5 !Radius
+  v0=0.01 !Reference velocity
+  
+!Loop over the grid
   do k=1,kx
      do j=1,jx
         do i=1,ix
         
-        radius=dsqrt(y(j)**2+x(i)**2)
+        radius=dsqrt(y(j)**2+x(i)**2) !calulate radius
         
+        !Set the values to zero for unused properties
         b_x(i,j,k)=0.d0
         b_y(i,j,k)=0.d0
         vy_m(i,j,k)=0.d0
         vz_m(i,j,k)=0.d0
         vy_h(i,j,k)=0.d0
         vz_h(i,j,k)=0.d0
-        if (radius .GT. 2.0*r0) then
+        if (radius .GT. 2.0*r0) then !If you are outside the tube: 
           ro_m(i,j,k)=n0down
           ro_h(i,j,k)=nndown
           P_m(i,j,k)=ppdown
@@ -218,21 +229,16 @@ print*,b0up,b0down
           enddo
           Nexcite(i,j,k,n_levels+1)=Nexcitedown(n_levels+1)
 
-        else
+        else !Inside the tube using tanh to smoothly bridge levels
           ro_m(i,j,k)=n0up+(n0down-n0up)*(tanh((radius-r0)/w_lay)+1.0)*0.5d0
           ro_h(i,j,k)=nnup+(nndown-nnup)*(tanh((radius-r0)/w_lay)+1.0)*0.5d0
           P_m(i,j,k) =ppup+(ppdown-ppup)*(tanh((radius-r0)/w_lay)+1.0)*0.5d0
           P_h(i,j,k) =pnup+(pndown-pnup)*(tanh((radius-r0)/w_lay)+1.0)*0.5d0
-          !print*,ro_h(i,j,k),ro_m(i,j,k),P_h(i,j,k),P_m(i,j,k)
-          !b_z(i,j,k) =b0up+(b0down-b0up)*(tanh((radius-r0)/w_lay)+1.0)*0.5d0
           if (flag_pip .eq. 0) then
               b_z(i,j,k)=dsqrt(2.0*(0.5*b0down**2+ppdown/5.0*3.0/ppup - 3.0/5.0*P_m(i,j,k)/ppup))
           else
               b_z(i,j,k)=dsqrt(2.0*(0.5*b0down**2+(ppdown+pndown)/5.0*3.0/ppup - 3.0/5.0*(P_m(i,j,k)+P_h(i,j,k))/ppup))
           endif
-
-          !vx_m(i,j,k)=v0*(tanh((radius-r0)/w_lay)+1.0)*0.5d0
-          !vx_h(i,j,k)=v0*(tanh((radius-r0)/w_lay)+1.0)*0.5d0
 
           do ii=1,n_levels
   	          Nexcite(i,j,k,ii)=Nexciteup(ii)+(Nexcitedown(ii)-Nexciteup(ii))*(tanh((radius-r0)/w_lay)+1.0)*0.5d0
@@ -241,22 +247,15 @@ print*,b0up,b0down
           -Nexciteup(n_levels+1))*(tanh((radius-r0)/w_lay)+1.0)*0.5d0
         endif
 
+        !Some velocity profile
         vx_m(i,j,k)=0.5d0*(dsin(3.14d0*(z(k))/10.d0))*v0*(1.0-tanh((radius-r0)/w_lay))
         vx_h(i,j,k)=0.5d0*(dsin(3.14d0*(z(k))/10.d0))*v0*(1.0-tanh((radius-r0)/w_lay))
         
-!vy_h(i,j,k)=0.01d0*(harvest((k-1)*jx*ix+(j-1)*ix+i)-0.5d0)
-!*dcos(x(i)*2.d0*pi)*exp(-2.d0*pi*abs(y(j))) &
-!                 *(-0.5d0*dtanh((dabs(y(j))-1.5d0)/0.15d0)+0.5d0 )
-!*(harvest((k-1)*jx*ix+(j-1)*ix+i)-0.5d0)
-        !vy_m(i,j,k)=0.01d0*(harvest((k-1)*jx*ix+(j-1)*ix+i)-0.5d0)
-!dcos(x(i)*2.d0*pi)*exp(-2.d0*pi*abs(y(j))) &
-!(harvest((k-1)*jx*ix+(j-1)*ix+i)-0.5d0)
-!*(harvest((k-1)*jx*ix+(j-1)*ix+i)-0.5d0)
         enddo
      enddo
   enddo
 
-!Normalisation
+!Normalisation to P_m=1/gamma, ro_m=1, such that the temperature of the plasma (T=gamma P/ ro) is 1
 P_m=P_m/5.0*3.0/ppup
 P_h=P_h/5.0*3.0/ppup
 ro_m=ro_m/n0up!*T0up
@@ -278,29 +277,10 @@ if ((flag_MHD .eq. 1) .and. (flag_PIP .ne. 1)) then
     print*,'Density range = ',maxval(ro_m),minval(ro_m)
     P_m=P_m!+P_h
     print*,'Pressure range = ',maxval(P_m),minval(P_m)
-    flag_IR=0
-    flag_rad=0
+    flag_IR=0 !make sure IR is turned off
+    flag_rad=0 !set radiation to zero, possibly not wanted
 endif
 
-!print*,nnup+(nndown-nnup)*(tanh(y(1)/w_lay)+1.0)*0.5d0
-!print*,sum(Nexcite(1,1,1,1:5)),ro_h(1,1,1)
-!print*,Nexcite(1,1,1,6),ro_m(1,1,1)
-
-!print*,ro_m+ro_h
-!print*,(P_m+P_h)/(ro_m+ro_h)*5.0/3.0!/(f_p_p_ini/f_p_ini*5.0/6.0)
-!print*,nndown+n0down,nnup+n0up
-!print*,f_p_pup,f_p_pdown/(pndown+ppdown)
-!print*,(P_m+P_h)/(ro_m+ro_h)*5.0/6.0!/(f_p_p_ini/f_p_ini*5.0/6.0)
-!print*,(P_m)/(ro_m)*5.0/6.0*(5.0/3.0*ppup/n0up)
-!print*,ppup/n0up*5.0/6.0,ppdown/n0down*5.0/6.0
-!print*,f_p_pup/f_pup,f_p_pdown/f_pdown
-
-!print*,'NEED TO DO SOME NORMALISATION'
-
-!stop
-
-!call get_col_ion_coeff(T0+0.d0*U_m(:,:,:,1),n0+0.d0*U_m(:,:,:,1),Gm_ion,Gm_rec)
-!call get_radrat_fixed(rad_temp,T0+0.d0*U_m(:,:,:,1),T0+0.d0*U_m(:,:,:,1),n0+0.d0*U_m(:,:,:,1),Gm_ion_rad,Gm_rec_rad)
   !!!========================================================
 
   !convert PV2cq and set that value to global variable 'U_h' and/or 'U_m'
