bfeab423e847a0e24280efd3bff1e63b232fc578
Astrosnow <b.snow@exeter.ac.uk>
2025-06-04 10:27:08 +0100

    Code to calculate photorecombination energy, WORK IN PROGRESS

---
+++ nlevscrips/average_photon_energy_recombination.py
@@ -0,0 +1,67 @@
+import numpy as np
+import astropy.constants as const
+import astropy.units as u
+import lightweaver as lw
+from lightweaver.rh_atoms import H_9_atom
+from scipy.integrate import quad
+import matplotlib.pyplot as plt
+
+Trad = 5777
+T_elec=6000.0 #electron temperature
+
+lambda_edges = [c.lambdaEdge for c in H_9_atom().continua]
+
+# def integrand_numerator(nu, Trad):
+#     return 1 / (np.exp(const.h.value * nu / (const.k_B.value * Trad)) - 1)
+
+# def integrand_denominator(nu, Trad):
+#     return 1 / nu * 1 / (np.exp(const.h.value * nu / (const.k_B.value * Trad)) - 1)
+
+def integrand_numerator_x(x, Trad):
+    return np.exp(-x) / (np.exp(-x*T_elec/Trad) - 1)
+
+def integrand_denominator_x(x, Trad):
+    return const.h.value / const.k_B.value / Trad / x * np.exp(-x) / (np.exp(-x*T_elec/Trad) - 1)
+
+def wave_to_x(wave, Trad):
+    nu = (wave << u.nm).to(u.Hz, equivalencies=u.spectral()).value
+    return const.h.value * nu / const.k_B.value / T_elec
+
+def get_energies(T_elec):
+    avg_freqs = []
+    for lambda_edge in lambda_edges:
+        num_int, num_int_err = quad(
+            integrand_numerator_x,
+            wave_to_x(lambda_edge, Trad),
+            np.inf,
+            args=(Trad,),
+            epsabs=1e-25
+        )
+        denom_int, denom_int_err = quad(
+            integrand_denominator_x,
+            wave_to_x(lambda_edge, Trad),
+            np.inf,
+            args=(Trad,),
+            epsabs=1e-25
+        )
+
+        if num_int_err / abs(num_int) > 1e-5 or denom_int_err / abs(denom_int) > 1e-5:
+            print(f"Error may be high for lambda_edge {lambda_edge:.2f} nm: {num_int_err:.3e}, {denom_int_err:.3e}")
+
+        avg_freqs.append(num_int / denom_int)
+
+    avg_energies = [(const.h * (f << u.Hz)).to(u.eV) for f in avg_freqs]
+    #for lambda_edge, energy in zip(lambda_edges, avg_energies):
+    #    print(f"{lambda_edge:.2f} nm: {energy.value:.2f} eV")
+    return avg_energies
+    
+T_elec_arr=np.linspace(6000,13000,1001)
+cooling_energy=np.zeros_like(T_elec_arr)
+for i in range(0,np.size(T_elec_arr)):
+    T_elec=T_elec_arr[i]
+    avg_energies=get_energies(T_elec)
+    print(T_elec,(avg_energies[0].value))
+    cooling_energy[i]=avg_energies[0].value
+    
+plt.plot(T_elec_arr,cooling_energy)
+plt.show()
